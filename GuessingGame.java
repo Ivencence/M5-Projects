2be2b539c9048fbe8d930ee330fdb26e3720f58c
IvaPandohova <ipandohova@gmail.com>
2024-12-05 22:35:38 +0200

    Update and rename Main to GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main {
+public class GuessingGame {
     public static void main(String[] args) {
         Random rand = new Random();
         int randomNum = rand.nextInt(100) + 1;
